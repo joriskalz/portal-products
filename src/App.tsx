f614d72e4e85a9884ae628b72a831f070eb2d997
joriskalz <joris@kalz.de>
2020-08-18 21:52:52 +0200

    styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,45 @@
 import React from "react";
+import StyledForm from "./styles";
 
 function App() {
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg mx-auto my-20">
-      <img
-        className="w-full"
-        src={require("./pexels-photo-2303781.jpeg")}
-        alt="Display"
-      />
-      <div className="px-6 py-4">
-        <div className="font-bold text-blue-800 text-xl mb-2">
-          Initial Tailwind Test
+    <>
+      <div className="max-w-sm rounded overflow-hidden shadow-lg mx-auto my-20">
+        <img
+          className="w-full"
+          src={require("./pexels-photo-2303781.jpeg")}
+          alt="Display"
+        />
+        <div className="px-6 py-4">
+          <div className="font-bold text-blue-800 text-xl mb-2">
+            Initial Tailwind Test
+          </div>
+          <p className="text-gray-700 text-base">
+            Credits:
+            https://www.smashingmagazine.com/2020/02/tailwindcss-react-project/
+          </p>
         </div>
-        <p className="text-gray-700 text-base">
-          Credits:
-          https://www.smashingmagazine.com/2020/02/tailwindcss-react-project/
-        </p>
+        <div className="px-6 py-4">
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+            #Software Engineer
+          </span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+            #Writter
+          </span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mt-2">
+            #Public Speaker
+          </span>
+        </div>
       </div>
-      <div className="px-6 py-4">
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #Software Engineer
-        </span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #Writter
-        </span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mt-2">
-          #Public Speaker
-        </span>
-      </div>
-    </div>
+      <StyledForm>
+        <form>
+          <input type="text" placeholder="Full name" />
+          <input type="text" placeholder="Email" />
+          <input type="text" placeholder="Password" />
+          <button>Sign In</button>
+        </form>
+      </StyledForm>
+    </>
   );
 }
 
